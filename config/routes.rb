a70b269deb32b1ec300f96a6213591cbdd1fe256
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-27 19:56:26 -0500

    add update action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/pillows' => 'pillows#index'
   get '/pillows/:id' => 'pillows#show'
   post '/pillows' => 'pillows#create'
+  patch '/pillows/:id' => 'pillows#update'
 end
--- frontend.rb
+++ frontend.rb
@@ -4,6 +4,7 @@ require 'unirest'
 # response = Unirest.get("localhost:3000/pillows")
 # p response.body
 
+
 # show action
 # p "Enter the id of the product you wish to see"
 # id = gets.chomp
@@ -11,13 +12,26 @@ require 'unirest'
 # response = Unirest.get("localhost:3000/pillows/#{id}")
 # p response.body
 
-# create action
 
-response = Unirest.post("localhost:3000/pillows", parameters: {
-  name: "Foam Pillow",
-  price: 45.54,
-  description: "pillow made of foam"
+# create action
+# response = Unirest.post("localhost:3000/pillows", parameters: {
+#   name: "Foam Pillow",
+#   price: 45.54,
+#   description: "pillow made of foam"
+#   }
+# )
+
+# p response.body
+
+
+# update action
+p "Enter the id of the product you wish to update"
+id = gets.chomp
+
+response = Unirest.patch("localhost:3000/pillows/#{id}", parameters: {
+  name: "Foamified Pillow",
+  price: 54.45,
+  description: "still a pillow made of foam"
   }
 )
-
 p response.body
