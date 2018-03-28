051cf21beadd485781b26e843c5af898f49a3467
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-27 20:21:52 -0500

    create as_json, tax and total methods for sleep model

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -1,3 +1,22 @@
 class Sleep < ApplicationRecord
   has_many :images
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      tax: tax,
+      total: total,
+      description: description
+    }
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
